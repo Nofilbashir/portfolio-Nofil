3287f933da30413ef60deee473a4bc736908d024
Nofilbashir <nofil.bashiir@gmail.com>
2024-09-16 02:38:01 -0700

    initial

---
+++ src/utils/Projects_data.js
@@ -0,0 +1,44 @@
+export const data = [
+  { id:7,name: "Step", year:"2024", type:"Ecommerce", category:"Project Management", type_class:"extra_small_class", class: "extra_small_class", img:"/step/1.png" },
+
+    { id:2,name: "Pets Maze", year:"2024", type:"PetTech", category:"Case_study",  type_class:"extra_small_class", class: "extra_small_class", img:"/petsmaze/hero.png" },
+    { id:3,name: "Fitness App", year:"2024", type:"FitTech", category:"Case_study", type_class:"extra_small_class", class: "extra_small_class" ,img:"/fitness/1.jpg"},
+    { id:4,name: "Violet", year:"2024", type:"Fintech", category:"Project Management", type_class:"extra_small_class", class: "extra_small_class" ,img:"/violet/1.png" },
+  { id:1,name: "Study Sync", year:"2024", type:"EdTech", category:"Case_study", type_class:"extra_small_class", class: "extra_small_class", img:"/studysync/1.png" },
+
+    // { id:5,name: "Globe Pay", year:"2024", type:"Fintech", category:"Case_study", type_class:"extra_small_class", class: "medium_size_class", img:"/globepay/1.jpg" },
+    
+    { id:6,name: "AI SaaS", year:"2024", type:"Tech", category:"Web_app", type_class:"extra_small_class", class: "extra_small_class" ,img:"/aisaas/1.png"},
+    // { id:8,name: "box2", year:"2024", type:"Photography", category:"Case_study",  type_class:"extra_small_class", class: "small_size_class", img:"/02hero.jpg" },
+    // { id:9,name: "box3", year:"2024", type:"Photography", category:"Web_app", type_class:"extra_small_class", class: "large_size_class" ,img:"/03hero.jpg"},
+    // { id:10,name: "box4", year:"2024", type:"Photography", category:"Case_study", type_class:"extra_small_class", class: "small_size_class" ,img:"/01hero.jpg" },
+    // { id:11,name: "box5", year:"2024", type:"Photography", category:"Mobile_app", type_class:"extra_small_class", class: "medium_size_class", img:"/02hero.jpg" },
+    // { id:12,name: "box6", year:"2024", type:"Photography", category:"Case_study", type_class:"extra_small_class", class: "large_size_class" ,img:"/03hero.jpg"},
+    // { id:13,name: "White Liners", year:"2024", type:"Photography", category:"Project Management", type_class:"extra_small_class", class: "medium_size_class", img:"/01hero.jpg" },
+    // { id:14,name: "box2", year:"2024", type:"Photography", category:"Project Management",  type_class:"extra_small_class", class: "small_size_class", img:"/02hero.jpg" },
+    // { id:15,name: "box3", year:"2024", type:"Photography", category:"Case_study", type_class:"extra_small_class", class: "large_size_class" ,img:"/03hero.jpg"},
+  ];
+
+  export const projectData = [
+    { id:1,name: "Study Sync",bg_color:"#7f6bff",backbtn:"white",
+      imgs:["/studysync/1.png","/studysync/2.png","/studysync/3.png","/studysync/4.png","/studysync/5.png","/studysync/6.png","/studysync/7.png","/studysync/8.png","/studysync/9.png","/studysync/10.png","/studysync/11.png","/studysync/12.png","/studysync/13.png","/studysync/14.png","/studysync/15.png","/studysync/16.png","/studysync/17.png","/studysync/18.png","/studysync/19.png","/studysync/20.png",]
+    },
+    { id:2,name: "Pets Maze",bg_color:"#f5f8ff",backbtn:"black",
+      imgs:["/petsmaze/hero.png","/petsmaze/1.png","/petsmaze/2.png","/petsmaze/3.png","/petsmaze/4.png","/petsmaze/5.png","/petsmaze/6.png","/petsmaze/7.png","/petsmaze/8.png","/petsmaze/9.png","/petsmaze/10.jpg","/petsmaze/11.jpg","/petsmaze/12.png","/petsmaze/13.png","/petsmaze/14.png","/petsmaze/15.png","/petsmaze/16.png","/petsmaze/17.png","/petsmaze/18.png","/petsmaze/19.jpg",]
+    },
+    { id:3,name: "Fitness App",bg_color:"#49586b",backbtn:"white",
+      imgs:["/fitness/1.jpg","/fitness/2.jpg","/fitness/3.jpg","/fitness/4.jpg","/fitness/5.jpg","/fitness/6.jpg","/fitness/7.jpg","/fitness/8.jpg","/fitness/9.jpg","/fitness/10.jpg","/fitness/11.jpg","/fitness/12.jpg","/fitness/13.jpg","/fitness/14.jpg","/fitness/15.jpg","/fitness/16.jpg","/fitness/17.jpg","/fitness/18.jpg","/fitness/19.jpg","/fitness/20.jpg","/fitness/21.jpg","/fitness/22.jpg","/fitness/23.jpg",]
+    },
+    { id:4,name: "Violet",bg_color:"#e9e4ff",backbtn:"black",
+      imgs:["/violet/1.png","/violet/2.png","/violet/3.png","/violet/4.png","/violet/5.png","/violet/6.png","/violet/7.png","/violet/8.png","/violet/9.png","/violet/10.png","/violet/11.png","/violet/12.png","/violet/13.png","/violet/14.png","/violet/15.png"]
+    },
+    // { id:5,name: "Globe Pay",bg_color:"#2b2b2b",
+    //   imgs:["/globepay/1.jpg","/globepay/2.jpg","/globepay/3.jpg","/globepay/4.jpg","/globepay/5.jpg","/globepay/6.jpg","/globepay/7.jpg","/globepay/8.jpg","/globepay/9.jpg","/globepay/10.jpg","/globepay/11.jpg","/globepay/12.jpg","/globepay/13.jpg","/globepay/14.jpg","/globepay/15.jpg","/globepay/16.jpg","/globepay/17.jpg","/globepay/18.jpg","/globepay/19.jpg","/globepay/20.jpg","/globepay/21.jpg","/globepay/22.jpg","/globepay/23.jpg","/globepay/24.jpg","/globepay/25.jpg","/globepay/26.jpg"]
+    // },
+    { id:6,name: "AI SaaS",bg_color:"#fffdf4",backbtn:"black",
+      imgs:["/aisaas/1.png","/aisaas/2.png"]
+    },
+    { id:7,name: "Step",bg_color:"#ffffff",backbtn:"black",
+      imgs:["/step/1.png","/step/2.png","/step/3.png","/step/4.png","/step/5.png","/step/6.png","/step/7.png","/step/8.png","/step/9.png","/step/10.png","/step/11.png","/step/12.png","/step/13.png","/step/14.png","/step/15.png","/step/16.png","/step/17.png","/step/18.png","/step/19.png","/step/20.png","/step/21.png","/step/22.png"]
+    },
+  ];
